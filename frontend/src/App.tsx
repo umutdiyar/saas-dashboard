e25c23edbf55c9ba9f4652062fffb9928c6ffd43
umutdiyar <umutdiyarbalci@gmail.com>
2026-01-20 00:49:08 +0300

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function App() {
+  return (
+    <div className="min-h-screen p-10">
+      <Button>shadcn OK</Button>
+    </div>
+  );
+}
